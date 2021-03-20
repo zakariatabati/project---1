5ba27b235be9aa5c7e84f3807ce7ce1bec081fc8
Zaki-94 <zakariatabati09@gmail.com>
2021-03-20 14:45:34 +0100

    Creat Application

---
+++ ZAKI74.py
@@ -0,0 +1,13 @@
+# Program For fixinf PGCD and PPCM FOR two numbers
+def pgcd(a,b):
+	if b == 0:
+		return a
+	else :
+		r = a%b
+		return pgcd(b,r)
+def ppcm(a,b):
+    if a == 0 or b == 0:
+        return 0
+    else :
+        return (a*b)//pgcd(a,b)
+
